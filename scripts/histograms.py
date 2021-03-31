b4e9f4caaee185a7bdd85fff426ebf55944d68af
Aniol Marti <aniol@aniolmarti.cat>
2021-03-31 16:24:24 +0200

    Work with 2 df instead of one to save memory

--- scripts/histograms.py
+++ scripts/histograms.py
@@ -21,33 +21,21 @@ def waveGenerator(file):
     # Find PE using a bash pipeline (faster)
     wave_pe = subprocess.run(["wave/wave_pe.sh", file], capture_output=True)
     # Dataframe for PE
-    df_pe = pd.read_csv(StringIO(wave_pe.stdout.decode('utf-8')), header=None, names=["PE"], dtype="int16")
+    df_pe = pd.read_csv(StringIO(wave_pe.stdout.decode('utf-8')), header=None, names=["PE"], dtype="int")
     # Dataframe for original samples
     df_samples = pd.DataFrame(np.fromfile(file, dtype="int16"), columns=["samples"], dtype="int16")
-    # Free memory asap
-    l = [df_pe, df_samples]
-    del df_pe, df_samples
-    # Dataframe with PE and samples
-    df = pd.concat(l, axis=1)
-    # Set dataframe columns names
-    df.columns = ["PE", "samples"]
-    return df
+    # Return both dataframes
+    return df_samples, df_pe
 
 def kmallGenerator(file):
     # Find PE using a bash pipeline (faster)
     kmall_pe = subprocess.run(["kmall/kmall_pe.sh", file], capture_output=True)
     # Dataframe for PE
-    df_pe = pd.read_csv(StringIO(kmall_pe.stdout.decode('utf-8')), header=None, names=["PE"], dtype="int8")
+    df_pe = pd.read_csv(StringIO(kmall_pe.stdout.decode('utf-8')), header=None, names=["PE"], dtype="int")
     # Dataframe for original samples
     df_samples = pd.DataFrame(np.fromfile(file, dtype="int8"), columns=["samples"], dtype="int8")
-    # Free memory asap
-    l = [df_pe, df_samples]
-    del df_pe, df_samples
-    # Dataframe with PE and samples
-    df = pd.concat(l, axis=1)
-    # Set dataframe columns names
-    df.columns = ["PE", "samples"]
-    return df
+    # Return both dataframes
+    return df_samples, df_pe
 
 if __name__ == "__main__":
     args = setupParser().parse_args()
@@ -57,24 +45,27 @@ if __name__ == "__main__":
     for file in args.files:
         print("Processing " + file + "...")
         # Get Pandas DataFrame
-        df = None
+        df_samples, df_pe = (None, None)
         if args.type == "wave":
-            df = waveGenerator(file)
+            df_samples, df_pe = waveGenerator(file)
         elif args.type == "kmall":
-            df = kmallGenerator(file)
+            df_samples, df_pe = kmallGenerator(file)
         else:
             sys.exit(-1)
         # Calculate prediction error probabilities.
-        probs = df["PE"].value_counts()/len(df["PE"])
+        probs = df_pe["PE"].value_counts() / len(df_pe["PE"])
         # Find theoretical ratio from Shannon entropy.
         bitsPerSample = 16 if args.type == "wave" else 8
         ratio = bitsPerSample/entropy(probs, base=2)
         print("Theoretical ratio \t %.3f" % (ratio))
-        # Plot histogram.
-        ax = df[["PE", "samples"]].plot.hist(bins=400, log=True, alpha=0.5)
+        # Plot histogram
+        n_bins = 400 if args.type == "wave" else 255
+        ax = plt.axes()
+        ax.hist(df_samples, bins=n_bins, log=True, alpha=0.5)
+        ax.hist(df_pe, bins=n_bins, log=True, alpha=0.5)
         ax.set_title("Histogram of prediction errors")
         ax.set_xlabel("Sample prediction errors")
         ax.set_ylabel("Number of samples")
-        #plt.savefig(file + "_hist.pdf")
         plt.show()
-        del df
+        #plt.savefig(file + "_hist.pdf")
+        del df_samples, df_pe
