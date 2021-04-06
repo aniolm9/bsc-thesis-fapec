781c37ceb4975b17cebef0b8c55e16db7bf9c687
Aniol Marti <aniol@aniolmarti.cat>
2021-04-06 17:22:24 +0200

    Add TRNLEN and clean comments

--- scripts/wave/wave_pe.sh
+++ scripts/wave/wave_pe.sh
@@ -2,20 +2,8 @@
 
 for FILE in "$@"
 do
-    ORDER=4
-    PERLEN=8192
-    : '
-    SIZE=$(stat --printf="%s" "$FILE")
-    let LINES=$SIZE/2-22
-
-    let m=$PERLEN/2+$ORDER
-    let bool=$SIZE%$PERLEN
-    if [[ $bool -ne 0 ]]; then
-        bool=1
-    fi
-    let firstLPC="($LINES-$SIZE%$PERLEN/4+1)*$bool"
-    let lastLPC="$firstLPC+($ORDER-1)*$bool"
-    '
-    #fapec -dtype 16 -signed -wave $ORDER 2 0 0 -per $PERLEN -mt 1 -o /dev/null -ow "$FILE" | grep PredErr | awk -v order="$ORDER" -v m="$m" '(NR-1)%m > order-1' | cut -d' ' -f2 | sed 1,22d | sed ${firstLPC},${lastLPC}d > "$FILE.pe"
-    fapec -dtype 16 -signed -wave $ORDER 2 0 0 -per $PERLEN -o /dev/null -ow "$FILE" | grep PredErr | cut -d' ' -f2
+    ORDER=8
+    PERLEN=4096
+    TRNLEN=4096
+    fapec -dtype 16 -signed -wave $ORDER 2 0 0 -per $PERLEN -trn $TRNLEN -o /dev/null -ow "$FILE" | grep PredErr | cut -d' ' -f2
 done
